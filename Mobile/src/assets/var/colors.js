2fcb102ef1aca14ec2754e4ef31c5ce33bfcfe3f
Caio Enrique Marcelli Freitas <cemf1996@gmail.com>
2020-06-05 17:55:02 -0300

    Fixed colors file to match previous pushes

--- Mobile/src/assets/var/colors.js
+++ Mobile/src/assets/var/colors.js
@@ -1,13 +1,5 @@
 export default {
   primary: '#1763E7',
-<<<<<<< HEAD
-  secondary: '#242424',
-  grey: '#F8F8F8',
-  backgroundGrey: '#F6F6F6',
-  white: '#FFFFFF',
-  statusGreen: '#17E73A',
-  ratingYellow: '#F8CC1B',
-=======
   cinzaEscuro: '#242424',
   cinza: '#888888',
   verde: '#17E73A',
@@ -15,5 +7,4 @@ export default {
   branco: '#FFFFFF',
   vermelho: '#F81B1B',
   background: '#F8F8F8',
->>>>>>> 2b4f94c6024400b9cc757da738f38d974740cef5
 }
