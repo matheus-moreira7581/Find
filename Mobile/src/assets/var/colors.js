cc3a96aaf8cd05bcba973f39175ca4d8c6d41110
Matheus <matheusmoreira7581@gmail.com>
2020-06-05 17:20:33 -0300

    Creating CompanyProducts, is a page with all products available of a selected company

--- Mobile/src/assets/var/colors.js
+++ Mobile/src/assets/var/colors.js
@@ -2,6 +2,7 @@ export default {
   primary: '#1763E7',
   secondary: '#242424',
   grey: '#F8F8F8',
+  textGrey: '#888888',
   white: '#FFFFFF',
   statusGreen: '#17E73A',
   ratingYellow: '#F8CC1B',
