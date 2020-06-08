53f4f39280b28f3758a8433098ee0ed23adfb49f
Rodrick <rs.strat@gmail.com>
2020-06-08 16:26:37 -0300

    Updated table

--- src/database/migrations/02_create_table_address.js
+++ src/database/migrations/02_create_table_address.js
@@ -1,9 +1,14 @@
-exports.up = function(knex) {
+exports.up = knex => knex.schema.createTable('addresses', table => {
 
-    
-}
+    table.increments('id')
+    table.string('street', 255).notNullable()
+    table.string('neighborhood',).notNullable()
+    table.integer('ad_number',).notNullable()
+    table.string('').nullable()
+    table.integer('zip', 11).notNullable()
+    table.string('img_url').nullable()
 
-exports.down = function(knex) {
-   
-    
-}
+})
+
+
+exports.down = knex => knex.schema.dropTable('addresses')
