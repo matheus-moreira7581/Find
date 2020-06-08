965a89951c88e89424007bc52c24bf10909a1160
Samuka Wenceslau <samukawens@gmail.com>
2020-06-08 15:27:01 -0300

    Create connection with knex
    knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development : {
+    client: "pg",
+    connection: {
+      database: "find_db",
+      user: "postgre",
+      password: "db123"
+    }
+  }
+};
