a03ea7d15815f2655dc54d850ddf417ff8d33a59
Samuka Wenceslau <samukawens@gmail.com>
2020-06-15 16:06:17 -0300

    Inserting values into sections table

---
+++ Backend/src/database/seeds/02_sections.js
@@ -0,0 +1,16 @@
+exports.seed = function(knex) {
+
+    return knex('sections').del()
+    .then(function() {
+
+        return knex('sections').insert([
+            {title: "Alimentação"},
+            {title: "Derivados"},
+            {title: "Saúde"},
+            {title: "Moda e beleza"},
+            {title: "Educação"},
+            {title: "Construção"},
+        ]);
+
+    });
+}
