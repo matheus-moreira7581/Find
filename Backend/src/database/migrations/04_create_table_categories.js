84157b80de5257921587adf58bd8d0ad47fbebaa
Samuka Wenceslau <samukawens@gmail.com>
2020-06-15 18:19:28 -0300

    Correcting "categories" table

--- Backend/src/database/migrations/04_create_table_categories.js
+++ Backend/src/database/migrations/04_create_table_categories.js
@@ -3,7 +3,7 @@ exports.up = knex =>  knex.schema.createTable('categories', table => {
     table.increments('id')
     table.string('title', 255).notNullable()
     table.string('img_url').nullable()
-    table.integer('id_section').notNullable()
+    table.integer('id_section').references('sections.id').notNullable().onDelete()
     
 });
 
