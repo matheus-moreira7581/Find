f8d80e3748e9e4372c108aa4c0deeb30c59a5186
Samuka Wenceslau <samukawens@gmail.com>
2020-06-15 21:26:17 -0300

    Update information

--- Backend/src/controller/CategoriesController.js
+++ Backend/src/controller/CategoriesController.js
@@ -1,16 +1,14 @@
 const knex = require('../database');
-const { index } = require('./ClientsController');
-
 
 module.exports = {
 
-    async index(request, response){
+    async index(request, response) {
 
         //const category = await knex('sections')
         //.join('categories', 'sections.id', 'categories.id_section')
         //.select('sections.name', 'categories.*')
 
-        const category = await knex('categories')
+        const category = await knex('categories');
 
         return response.json(category);
 
