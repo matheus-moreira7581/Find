1eb60d4a96e7c2908d3069b4a0178724a18985a6
Rodrick <rs.strat@gmail.com>
2020-06-22 21:49:37 -0300

    Single login
    AuthController.js

--- Backend/src/controller/AuthController.js
+++ Backend/src/controller/AuthController.js
@@ -3,24 +3,27 @@ const bcrypt = require('bcrypt')
 
 module.exports = {
 
-     // Login para Empresas (tela de login)
+     // Login para Usuários (empresa/cliente)
 
      async login(request, response, next) {
         try 
         {
         const { email, password} = request.body;
             
-        const user = await knex('company')
+        const client = await knex('clients')
+        .where({ email });
+
+        const company = await knex('company')
         .where({ email });
 
        
-       if (user.length == 0) {
+       if (client.length == 0 && company.length == 0) {
 
           return response.status(400).send('Cannot find user')
 
         } 
         
-        if(await bcrypt.compare(password, company[0].password)) {
+        if(await bcrypt.compare(password, client[0].password || company[0].password)) {
 
             response.send('Success')
 
