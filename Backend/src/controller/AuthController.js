c80a1106cc5bf838bfa47a87007d764cdddf6396
Samuka Wenceslau <samukawens@gmail.com>
2020-06-23 21:49:11 -0300

    Update code

--- Backend/src/controller/AuthController.js
+++ Backend/src/controller/AuthController.js
@@ -14,13 +14,10 @@ module.exports = {
           const client = await knex('clients')
           .where({ email });
           
-
           const company = await knex('company')
           .where({ email });
           
-
-
-       
+          
           if (client.length == 0 && company.length == 0) {
 
             return response.status(400).send('Cannot find user')
