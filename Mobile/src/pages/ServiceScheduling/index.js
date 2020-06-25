85b884b8157c8ff29c7de27aaac4da2cb0827445
Caio Enrique Marcelli Freitas <cemf1996@gmail.com>
2020-06-25 18:24:49 -0300

    Edited Service Scheduling screen

--- Mobile/src/pages/ServiceScheduling/index.js
+++ Mobile/src/pages/ServiceScheduling/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { SafeAreaView, View, TouchableWithoutFeedback, TouchableOpacity, Text, TextInput } from 'react-native';
+import { SafeAreaView, View, TouchableWithoutFeedback, TouchableOpacity, Text, TextInput, Keyboard } from 'react-native';
 
 import RoundedButton from '../../components/RoundedButton';
 import { MaterialIcons } from '@expo/vector-icons';
 
-import { adjustHorizontalMeasure } from '../../utils/adjustMeasures';
+import { adjustHorizontalMeasure, adjustVerticalMeasure } from '../../utils/adjustMeasures';
 
 import colors from '../../assets/var/colors';
 
@@ -19,13 +19,26 @@ const ServiceScheduling = () => {
                     <MaterialIcons name="arrow-back" size={adjustHorizontalMeasure(20)} color={colors.cinzaEscuro}/>
                 </TouchableOpacity>
                 <View style={styles.centeredContainer}>
-                    <Text>Local do Profissional</Text>
+                    <Text style={styles.headerText}>Local do Profissional</Text>
                 </View>
             </View>
-            <TouchableWithoutFeedback style={styles.bodyContainer}>
-                <View>
-                    
+            <TouchableWithoutFeedback style={styles.bodyContainer} onPress={()=>Keyboard.dismiss()}>
+                <View style={styles.bodyContainer}>
+                    <Text style={styles.inputTitle}>Quem irá até o local?</Text>
+                    <TextInput style={styles.input} placeholder="Digite o nome completo" placeholderTextColor={colors.cinza}/>
+                    <Text style={styles.text}>Agende um horário</Text>
+                    <Text style={styles.text}>Hoje</Text>
+                    <View style={styles.tempContainer}><Text>Componente vem aqui</Text></View>
+                    <View style={styles.buttonContainer}>
+                        <RoundedButton
+                            text="Continuar"
+                            width={adjustHorizontalMeasure(256)}
+                            height={adjustVerticalMeasure(50)}
+                            selected={true}
+                        />
+                    </View>
                 </View>
+                
             </TouchableWithoutFeedback>
         </SafeAreaView>
     );
--- Mobile/src/pages/ServiceScheduling/styles.js
+++ Mobile/src/pages/ServiceScheduling/styles.js
@@ -1,5 +1,7 @@
 import { StyleSheet } from 'react-native';
 
+import Constants from 'expo-constants';
+
 import colors from '../../assets/var/colors';
 import fonts from '../../assets/var/fonts';
 
@@ -7,12 +9,73 @@ import { adjustHorizontalMeasure, adjustVerticalMeasure } from '../../utils/adju
 import adjustFontSize from '../../utils/adjustFontSize';
 
 const styles = StyleSheet.create({
-    screenContainer:{},
-    headerContainer:{},
-    centeredContainer:{},
-    headerText:{},
-    bodyContainer:{},
-    backButton:{},
+    screenContainer:{
+        flex: 1,
+        top: Constants.statusBarHeight,
+    },
+    headerContainer:{
+        backgroundColor: colors.background,
+        height: adjustVerticalMeasure(98.5),
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        borderBottomWidth: 1,
+        borderBottomColor: colors.bordarCinza,
+    },
+    centeredContainer:{
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: adjustHorizontalMeasure(13.5),
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    headerText:{
+        fontFamily: fonts.montserratBold,
+        fontSize: adjustFontSize(20),
+    },
+    bodyContainer:{
+        flex: 1,
+        backgroundColor: colors.background,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    inputTitle:{
+        alignSelf: 'flex-start',
+        marginLeft: adjustHorizontalMeasure(24),
+        marginBottom: adjustVerticalMeasure(6),
+        marginTop: adjustVerticalMeasure(62.5),
+        fontFamily: fonts.montserratBold,
+        fontSize: adjustFontSize(15),
+    },
+    input:{
+        alignSelf: 'stretch',
+        marginHorizontal: adjustHorizontalMeasure(24),
+        backgroundColor: colors.textInput,
+        borderRadius: 7,
+        marginBottom: adjustVerticalMeasure(41),
+        fontFamily: fonts.montserratRegular,
+        fontSize: adjustFontSize(13),
+        paddingVertical: adjustVerticalMeasure(3),
+        paddingLeft: adjustHorizontalMeasure(15),
+    },
+    text:{
+        fontFamily: fonts.montserratBold,
+        fontSize: adjustFontSize(15),
+        marginBottom: adjustVerticalMeasure(37),
+    },
+    buttonContainer:{
+        height: adjustVerticalMeasure(354),
+        justifyContent: 'center',
+    },
+    tempContainer:{
+        backgroundColor: '#C1C1CC',
+        height: adjustVerticalMeasure(139),
+        marginBottom: 'auto',
+    },
+    backButton:{
+        marginLeft: adjustHorizontalMeasure(24),
+        marginBottom: adjustVerticalMeasure(18.5),
+    },
 });
 
 export default styles;
