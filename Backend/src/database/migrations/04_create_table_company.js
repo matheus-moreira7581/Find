c534a66806ad58cea6ca92adfc3a8ad9357b797d
Samuka Wenceslau <samukawens@gmail.com>
2020-06-25 19:34:03 -0300

    Refactoring code

--- Backend/src/database/migrations/04_create_table_company.js
+++ Backend/src/database/migrations/04_create_table_company.js
@@ -1,15 +1,16 @@
 exports.up = knex => knex.schema.createTable('company', table => {
 
     table.increments('id')
+    table.integer('id_categories').references('categories.id').notNullable().onDelete()
     table.string('name', 255).notNullable()
     table.string('cpf').notNullable()
     table.string('email').unique().notNullable()
     table.string('password').notNullable()
-    table.enu('type', ['service', 'product']).notNullable()
     table.string('date_birth').notNullable()
     table.string('address').notNullable()
+    table.enu('type', ['service', 'product']).notNullable()
+    table.json('hours_schedule').nullable()
     table.string('img_url').nullable()
-    table.integer('id_categories').references('categories.id').notNullable().onDelete()
 
 })
 
