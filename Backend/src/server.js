ae4fb51f91d6772c26f587d1aa5d45583628f7d3
Matheus <matheusmoreira7581@gmail.com>
2020-06-25 19:22:13 -0300

    Trying to fix heroku response error

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,49 +1,48 @@
-const express = require('express')
-const bcrypt = require('bcrypt')
-const clientRoutes = require('./routes/client')
-const addressRoutes = require('./routes/address')
-const categoryRoutes = require('./routes/category')
-const companyRoutes = require('./routes/company')
-const productsRoutes = require('./routes/product')
-const authRoutes = require('./routes/auth')
-const ordersRoutes = require('./routes/order')
-const cors = require('cors')
-
-
+const express = require("express");
+const bcrypt = require("bcrypt");
+const clientRoutes = require("./routes/client");
+const addressRoutes = require("./routes/address");
+const categoryRoutes = require("./routes/category");
+const companyRoutes = require("./routes/company");
+const productsRoutes = require("./routes/product");
+const authRoutes = require("./routes/auth");
+const ordersRoutes = require("./routes/order");
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
-app.use(express.json())
-app.use(clientRoutes)
-app.use(addressRoutes)
-app.use(categoryRoutes)
-app.use(companyRoutes)
-app.use(productsRoutes)
-app.use(authRoutes)
-app.use(ordersRoutes)
+app.use(express.json());
+app.use(clientRoutes);
+app.use(addressRoutes);
+app.use(categoryRoutes);
+app.use(companyRoutes);
+app.use(productsRoutes);
+app.use(authRoutes);
+app.use(ordersRoutes);
 
-// not Found
+app.use(logErrors);
+app.use(clientErrorHandler);
+app.use(errorHandler);
 
-app.use((request, response, next) => {
+function logErrors(err, req, res, next) {
+  console.error(err.stack);
+  next(err);
+}
 
-    const error = new Error('Não encontrado')
-    response.status(500).send()
-    next(error)
+function clientErrorHandler(err, req, res, next) {
+  if (req.xhr) {
+    res.status(500).send({ error: "Something failed!" });
+  } else {
+    next(err);
+  }
+}
 
-});
-
-// pegar todos os erros
-
-app.use((error, request, response, next) => {
-
-    response.status(error.status || 500)
-
-    response.json({ error: error.message })
-
-});
+function errorHandler(err, req, res, next) {
+  res.status(500);
+  res.render("error", { error: err });
+}
 
 const port = process.env.PORT || 3333;
-
 
 app.listen(port, () => console.log("Servidor funcionando!"));
