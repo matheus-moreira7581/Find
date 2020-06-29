d34f308fac428b70dbc1c617e6b570036f0b9443
Samuka Wenceslau <samukawens@gmail.com>
2020-06-29 13:50:27 -0300

    delete mobile

--- Mobile/src/assets/var/fonts.js
+++
@@ -1,7 +0,0 @@
-export default {
-  montserratRegular: 'Montserrat_400Regular',
-  montserrat: 'Montserrat_500Medium',
-  montserratSemiBold: 'Montserrat_600SemiBold',
-  montserratBold: 'Montserrat_700Bold',
-
-}
