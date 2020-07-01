5066922b45d9070485f57b3029470dcfe8f969f4
Matheus <matheusmoreira7581@gmail.com>
2020-07-01 18:14:53 -0300

    Creating MarketBag screen

---
+++ Mobile/src/pages/MarketBag/styles.js
@@ -0,0 +1,62 @@
+import { StyleSheet } from 'react-native';
+
+import  Constants  from 'expo-constants';
+
+import fonts from '../../assets/var/fonts';
+import colors from '../../assets/var/colors';
+
+import adjustFontSize from '../../utils/adjustFontSize';
+
+import { adjustHorizontalMeasure, adjustVerticalMeasure } from '../../utils/adjustMeasures';
+
+const styles = StyleSheet.create({
+  headerContainer:{
+    justifyContent: 'flex-end',
+    height: adjustVerticalMeasure(98.5),
+    borderBottomWidth: 1,
+    borderBottomColor: colors.bordarCinza,
+  },
+  centeredContainer:{
+      position: 'absolute',
+      bottom: adjustVerticalMeasure(13.5),
+      right: 0,
+      left: 0,
+      justifyContent: 'center',
+      alignItems: 'center'
+  },
+  categoryText:{
+      fontFamily: fonts.montserratBold,
+      fontSize: adjustFontSize(20),
+      color: colors.cinzaEscuro,
+  },
+  backButton:{
+    marginLeft: adjustHorizontalMeasure(24),
+    marginBottom: adjustVerticalMeasure(18.5),
+  },
+  contentContainer: {
+    height: adjustVerticalMeasure(673.5),
+    width: '100%',
+  },
+  listContainer: {
+    height: adjustVerticalMeasure(528),
+    alignItems: 'center',
+  },
+  buttonsContainer: {
+    alignItems: 'center',
+  },
+  deleteBottom: {
+    marginTop: adjustVerticalMeasure(15),
+    width: '100%',
+    height: adjustVerticalMeasure(40),
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: adjustVerticalMeasure(8),
+  },
+  deleteText: {
+    fontFamily: fonts.montserratBold,
+    fontSize: adjustFontSize(12),
+    color: colors.vermelho,
+  }
+});
+
+export default styles;
