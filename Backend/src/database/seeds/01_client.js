69354d9c66c8264bb2f90da3a64f1334feae0bcd
Matheus <matheusmoreira7581@gmail.com>
2020-07-01 11:05:16 -0300

    Adding a few seeds on company and client

--- Backend/src/database/seeds/01_client.js
+++ Backend/src/database/seeds/01_client.js
@@ -6,7 +6,8 @@ exports.seed = function(knex) {
         return knex('clients').insert([
             {name: 'Fulano', email:"fulano@mail.com", password:"789456", cell:"139876543", img_url:"my-photo"},
             {name: 'Ciclano', email:"ciclano@mail.com", password:"456123", cell:"139123456", img_url:"my-photo"},
-            {name: 'Beltrano', email:"beltrano@mail.com", password:"123789", cell:"139321654", img_url:"my-photo"}
+            {name: 'Beltrano', email:"beltrano@mail.com", password:"123789", cell:"139321654", img_url:"my-photo"},
+            {name: 'Lucas', email:"lucas@gmail.com", password:"$2b$10$RIreFY3xsYTO9OFq94ZJZeuf/jn8/izsoNXxs8C4SdSNXDvVStNO6", cell:"(11) 98582-4725", img_url:"my-photo"},
         ]);
 
     });
--- Backend/src/database/seeds/04_company.js
+++ Backend/src/database/seeds/04_company.js
@@ -7,7 +7,9 @@ exports.seed = function(knex) {
             {name: 'Aço puro', email:"aço@mail.com", password:"987654", type:"service",
             cpf: "1234567891", date_birth:"10/10/1997", address:"av. são cristóvão", img_url:"my-photo", id_categories: 1},
             {name: 'Ouro puro', email:"ouro@mail.com", password:"123456", type:"product",
-            cpf: "1234567891", date_birth:"10/10/1997", address:"av. são cristóvão", img_url:"my-photo", id_categories: 1}
+            cpf: "1234567891", date_birth:"10/10/1997", address:"av. são cristóvão", img_url:"my-photo", id_categories: 1},
+            {name: 'Tasty Pizza', email:"tastypizza@gmail.com", password:"$2b$10$PBynjWrJdb28w.0xBfJMTujiJAoIA5h/EIRXHcswtwx5hTApJIh3O", type:"product",
+            cpf: "24586875469", date_birth:"10/10/1992", address:"R. José da Costa Monteiro, 625", img_url:"my-photo", id_categories: 2}
         ]);
 
     });
