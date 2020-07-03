f51b0fd978b7e6d0df1818af519af850f25a30c9
Rodrick <rs.strat@gmail.com>
2020-07-03 13:37:06 -0300

    Image URL in seeds

--- Backend/src/database/seeds/01_client.js
+++ Backend/src/database/seeds/01_client.js
@@ -4,10 +4,7 @@ exports.seed = function(knex) {
     .then(function() {
 
         return knex('clients').insert([
-            {name: 'Fulano', email:"fulano@mail.com", password:"789456", cell:"139876543", img_url:"my-photo"},
-            {name: 'Ciclano', email:"ciclano@mail.com", password:"456123", cell:"139123456", img_url:"my-photo"},
-            {name: 'Beltrano', email:"beltrano@mail.com", password:"123789", cell:"139321654", img_url:"my-photo"},
-            {name: 'Lucas', email:"lucas@gmail.com", password:"$2b$10$RIreFY3xsYTO9OFq94ZJZeuf/jn8/izsoNXxs8C4SdSNXDvVStNO6", cell:"(11) 98582-4725", img_url:"my-photo"},
+            {name: 'Lucas', email:"lucas@gmail.com", password:"$2b$10$RIreFY3xsYTO9OFq94ZJZeuf/jn8/izsoNXxs8C4SdSNXDvVStNO6", cell:"(11) 98582-4725", img_url:"https://res.cloudinary.com/dbehp0gsj/image/upload/v1593793320/uploads/lucas_bjwm0g.png"},
         ]);
 
     });
