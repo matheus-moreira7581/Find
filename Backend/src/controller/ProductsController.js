d8764ce6a76cb65c3115ac56029d388159392263
Matheus <matheusmoreira7581@gmail.com>
2020-07-06 17:15:27 -0300

    Fixing product creation for when a image was not sent

--- Backend/src/controller/ProductsController.js
+++ Backend/src/controller/ProductsController.js
@@ -36,10 +36,12 @@ module.exports = {
             const item = [{ name, description, price, limit_time, id_company }];
 
             const product = item.map(element => {
-                return {
-                    "img_url": urls[0].url,
-                    ...element
+                if(urls.length > 0) {
+                    return {
+                        "img_url": urls[0].url,...element
+                    }
                 }
+                else return {...element}
             })
 
             await knex('products').insert(product);
