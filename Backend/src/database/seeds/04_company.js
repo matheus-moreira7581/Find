85cd9ef8e204536fa0a72d12272850cd456c314c
Matheus <matheusmoreira7581@gmail.com>
2020-07-07 16:33:06 -0300

    Creating function to fetch available hours

--- Backend/src/database/seeds/04_company.js
+++ Backend/src/database/seeds/04_company.js
@@ -7,7 +7,8 @@ exports.seed = function(knex) {
             {name: 'Tasty Pizza', email:"tastypizza@gmail.com", password:"$2b$10$PBynjWrJdb28w.0xBfJMTujiJAoIA5h/EIRXHcswtwx5hTApJIh3O", type:"product",
             cpf: "24586875469", date_birth:"10/10/1992", address:"R. José da Costa Monteiro, 625", img_url:"https://res.cloudinary.com/dbehp0gsj/image/upload/v1593793176/uploads/tasty_pizza_xfb7ru.png", id_categories: 2},
             {name: "Uncle B's - Barbershop", email:"uncleb@gmail.com", password:"$2b$10$PBynjWrJdb28w.0xBfJMTujiJAoIA5h/EIRXHcswtwx5hTApJIh3O", type:"service",
-            cpf: "54862458798", date_birth:"10/10/1991", address:"R. José da Costa Monteiro, 625", img_url:"https://res.cloudinary.com/dbehp0gsj/image/upload/v1594050778/uploads/Uncle_zpipvm.png", id_categories: 15}
+            cpf: "54862458798", date_birth:"10/10/1991", address:"R. José da Costa Monteiro, 625", hours_schedule:["13:00", "13:30", "14:00", "14:30", "15:00", "15:30"], 
+            img_url:"https://res.cloudinary.com/dbehp0gsj/image/upload/v1594050778/uploads/Uncle_zpipvm.png", id_categories: 15}
         ]);
 
     });
